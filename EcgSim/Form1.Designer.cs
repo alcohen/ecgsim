582bedebe2b45f465dc30a6d536371d7ea224480
alcohen <amc79@cornell.edu>
2013-08-08 09:32:45 -0400

    Initial Commit

---
+++ EcgSim/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace EcgSim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnFileSelect = new System.Windows.Forms.Button();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbComPort = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.dlgFileSelect = new System.Windows.Forms.OpenFileDialog();
+            this.serOut = new System.IO.Ports.SerialPort(this.components);
+            this.txtSerIn = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClearBox = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFileSelect
+            // 
+            this.btnFileSelect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFileSelect.Enabled = false;
+            this.btnFileSelect.Location = new System.Drawing.Point(330, 35);
+            this.btnFileSelect.Name = "btnFileSelect";
+            this.btnFileSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnFileSelect.TabIndex = 0;
+            this.btnFileSelect.Text = "Select...";
+            this.btnFileSelect.UseVisualStyleBackColor = true;
+            this.btnFileSelect.Click += new System.EventHandler(this.btnFileSelect_Click);
+            // 
+            // lblFile
+            // 
+            this.lblFile.Location = new System.Drawing.Point(0, 43);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(38, 24);
+            this.lblFile.TabIndex = 1;
+            this.lblFile.Text = "File";
+            this.lblFile.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtFile
+            // 
+            this.txtFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFile.Enabled = false;
+            this.txtFile.Location = new System.Drawing.Point(44, 38);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(280, 20);
+            this.txtFile.TabIndex = 2;
+            this.txtFile.TextChanged += new System.EventHandler(this.txtFile_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(-1, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Com";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // cmbComPort
+            // 
+            this.cmbComPort.FormattingEnabled = true;
+            this.cmbComPort.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14"});
+            this.cmbComPort.Location = new System.Drawing.Point(44, 9);
+            this.cmbComPort.Name = "cmbComPort";
+            this.cmbComPort.Size = new System.Drawing.Size(82, 21);
+            this.cmbComPort.TabIndex = 4;
+            this.cmbComPort.SelectionChangeCommitted += new System.EventHandler(this.cmbComPort_SelectionChangeCommitted);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(44, 64);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(126, 63);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // dlgFileSelect
+            // 
+            this.dlgFileSelect.FileName = "openFileDialog1";
+            // 
+            // serOut
+            // 
+            this.serOut.RtsEnable = true;
+            this.serOut.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serOut_DataReceived);
+            // 
+            // txtSerIn
+            // 
+            this.txtSerIn.Location = new System.Drawing.Point(44, 128);
+            this.txtSerIn.Multiline = true;
+            this.txtSerIn.Name = "txtSerIn";
+            this.txtSerIn.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtSerIn.Size = new System.Drawing.Size(94, 84);
+            this.txtSerIn.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "File";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // btnClearBox
+            // 
+            this.btnClearBox.Location = new System.Drawing.Point(155, 128);
+            this.btnClearBox.Name = "btnClearBox";
+            this.btnClearBox.Size = new System.Drawing.Size(75, 23);
+            this.btnClearBox.TabIndex = 8;
+            this.btnClearBox.Text = "Clear";
+            this.btnClearBox.UseVisualStyleBackColor = true;
+            this.btnClearBox.Click += new System.EventHandler(this.btnClearBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 262);
+            this.Controls.Add(this.btnClearBox);
+            this.Controls.Add(this.txtSerIn);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.cmbComPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.btnFileSelect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFileSelect;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbComPort;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.OpenFileDialog dlgFileSelect;
+        private System.IO.Ports.SerialPort serOut;
+        private System.Windows.Forms.TextBox txtSerIn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClearBox;
+    }
+}
+
---
+++ EcgSim/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WfdbCsharpWrapper;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+namespace EcgSim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            //using (var record = new Record("c:\\users\\Al\\Dropbox\\Medical Projects\\ECG Simulator\\EcgSim\\EcgSim\\data\\100s"))
+            using (var record = new Record("data/aami-ec13/aami3a"))
+            //using (var record = new Record(txtFile.Text.Substring(2, txtFile.Text.Length - 6)))
+            {
+                record.Open();
+                //next line added by Al as test
+                Frequency.InputFrequency = 125;
+                
+                var samples = record.GetSamples(1000);
+                int val;
+                string outword;
+                serOut.BaudRate = 38400;
+                serOut.Parity = Parity.None;
+                serOut.DataBits = 8;
+                serOut.StopBits = StopBits.One;
+                serOut.Handshake = Handshake.None;
+                serOut.Open();
+                foreach (var s in samples)
+                {
+                    for (int i = 0; i < s.Length; i++)
+                    {
+                        //Console.Write(s[i].ToString());
+                        //Console.Write(string.Format("D{0000}\t", s[i]));
+                        val = s[i];
+                        outword = "D" + val.ToString("0000");
+                        Console.Write(outword);
+                        serOut.Write(outword);
+                    }
+                    Console.WriteLine();
+                    Thread.Sleep(5);
+                    Application.DoEvents();
+                    if (btnStart.Enabled)
+                    {
+                        break;
+                    }
+                }
+            }
+            serOut.Close();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            Wfdb.Quit();
+        }
+
+        private void cmbComPort_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            //txtFile.Enabled = true;
+            //tnFileSelect.Enabled = true;
+            serOut.PortName = "COM" + cmbComPort.SelectedItem.ToString().Trim();
+            btnStart.Enabled = true;
+        }
+
+        private void txtFile_TextChanged(object sender, EventArgs e)
+        {
+            //check if a valid file...
+            if (File.Exists(txtFile.Text))
+            {
+                btnStart.Enabled = true;
+                btnStop.Enabled = true;
+            }
+            else
+            {
+                btnStart.Enabled = false;
+                btnStop.Enabled = false;
+            }
+        }
+
+        private void btnFileSelect_Click(object sender, EventArgs e)
+        {
+            dlgFileSelect.Filter = "Physionet header files (*.head)|*.hea|All files (*.*)|*.*";
+            dlgFileSelect.FilterIndex = 1;
+            if (dlgFileSelect.ShowDialog() == DialogResult.OK)
+            {
+                txtFile.Text = dlgFileSelect.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //cmbComPort.Items = serOut
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            serOut.Close();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void serOut_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            txtSerIn.Text += e.ToString();
+        }
+
+        private void btnClearBox_Click(object sender, EventArgs e)
+        {
+            txtSerIn.Text = "";
+        }
+
+    }
+}
